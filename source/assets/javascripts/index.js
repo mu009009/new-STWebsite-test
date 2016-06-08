5f8704821e63d2c19017f8ee569ea27cbefcb6a0
hamholla <hamhollett@gmail.com>
2016-06-08 16:30:31 -0400

    instagram feed setup and templated

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,2 +1,16 @@
 import Bootstrap from 'bootstrap'
 
+import Instafeed from 'instafeed.js'
+
+    var userFeed = new Instafeed({
+    target: 'instafeed',
+    get: 'user',
+    userId: '2317298185',
+    clientId: 'd9ceeb2a8e974c9d871f28f4cd1a4bc5',
+    accessToken: '2317298185.d9ceeb2.ea81a600401e462ea4cddb95793aa529',
+    limit: '5',
+    sortBy: 'most-recent',
+    resolution: 'standard_resolution',
+    template: '<div style="width:20%; display: inline-block;"><a href="{{link}}"><img src="{{image}}" style="width:100%;"/></a></div>'
+  });
+  userFeed.run();
