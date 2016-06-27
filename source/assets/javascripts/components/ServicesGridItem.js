3776bb0bc87f77c8a8cd3e52da168c0516f98f4b
hamholla <hamhollett@gmail.com>
2016-06-27 13:20:36 -0400

    fixed asset path for capabilities icons

--- source/assets/javascripts/components/ServicesGridItem.js
+++ source/assets/javascripts/components/ServicesGridItem.js
@@ -6,7 +6,7 @@ export default class ServicesGrid extends React.Component {
 		return (
 			<div className={`service ${activeItem ? 'active' : 'inactive'} `}>
 				<div className="service-icon">
-					<img src={`../assets/images/services/${item.icon}`}/>
+					<img src={`assets/images/services/${item.icon}`}/>
 				</div>
 				<div className="service-label">
 					{item.name}
