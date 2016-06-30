f2c16f5bcb039731585ae9c665e584f58420ced2
hamholla <hamhollett@gmail.com>
2016-06-30 17:59:13 -0400

    tks logo polish, medium feed responsive, filter list caps, maps links, links for homepage

--- source/assets/javascripts/components/FilterList.js
+++ source/assets/javascripts/components/FilterList.js
@@ -5,11 +5,11 @@ const FilterList = ({ onFilterSelect }) => {
 		<div>
 			<div className="subhead filter-header"> I need to...</div>
 			<div className="filter-list">
-				<button onClick={() => onFilterSelect('NEW_MARKET')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">01</p>introduce my product to a new market.</button>
-				<button onClick={() => onFilterSelect('EXTEND_REACH')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">02</p>extend the reach of my sales force.</button>
-				<button onClick={() => onFilterSelect('LEADS')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">03</p>deliver more qualified leads to my sales team.</button>
-				<button onClick={() => onFilterSelect('FUNNEL')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">04</p>grow the funnel.</button>
-				<button onClick={() => onFilterSelect('GAP')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">05</p>close the gap in field/territory coverage.</button>
+				<button onClick={() => onFilterSelect('NEW_MARKET')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">01</p>Introduce my product to a new market.</button>
+				<button onClick={() => onFilterSelect('EXTEND_REACH')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">02</p>Extend the reach of my sales force.</button>
+				<button onClick={() => onFilterSelect('LEADS')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">03</p>Deliver more qualified leads to my sales team.</button>
+				<button onClick={() => onFilterSelect('FUNNEL')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">04</p>Grow the funnel.</button>
+				<button onClick={() => onFilterSelect('GAP')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">05</p>Close the gap in field/territory coverage.</button>
 			</div>
 			<button onClick={() => onFilterSelect('ALL')} className="btn btn-primary capabilities-link">show all services</button>
 		</div>
--- source/assets/javascripts/mediumFeed.js
+++ source/assets/javascripts/mediumFeed.js
@@ -5,7 +5,7 @@ if ($('#rss-feeds').length>0) {
 		{
 			limit: 6,
 			layoutTemplate: "<div class='row feed-container'>{entries}</div>",
-			entryTemplate: '<div class="col-md-6 feed-item"><a target="_blank" href="{url}"><div class="post-image-container">{teaserImage}</div><div class="post-title highlighted-content">{title}</div><div class="post-body body-copy-small">{shortBodyPlain}...</div></a></div>',
+			entryTemplate: '<div class="col-md-6 col-lg-4 col-sm-6 feed-item"><a target="_blank" href="{url}"><div class="post-image-container">{teaserImage}</div><div class="post-title highlighted-content">{title}</div><div class="post-body body-copy-small">{shortBodyPlain}...</div></a></div>',
 			ssl: true
 		}
 	);
