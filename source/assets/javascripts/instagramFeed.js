183fafc88de5a4dada21658cd0baa921cdc11a4f
hamholla <hamhollett@gmail.com>
2016-07-01 14:15:39 -0400

    isntafeed responsive sizing, load more photos, scroll on smaller screens

--- source/assets/javascripts/instagramFeed.js
+++ source/assets/javascripts/instagramFeed.js
@@ -7,10 +7,10 @@ if ($('#instafeed').length>0){
 			userId: '2317298185',
 			clientId: 'd9ceeb2a8e974c9d871f28f4cd1a4bc5',
 			accessToken: '2317298185.d9ceeb2.ea81a600401e462ea4cddb95793aa529',
-			limit: '5',
+			limit: '20',
 			sortBy: 'most-recent',
 			resolution: 'standard_resolution',
-			template: '<div style="width:20%; display: inline-block;"><a href="{{link}}"><img src="{{image}}" style="width:100%;"/></a></div>'
+			template: '<a href="{{link}}" class="instaphoto"><img src="{{image}}" style="height: 200px"/></a>'
 	});
 	userFeed.run();
 }
