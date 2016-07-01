c98c56f69d583b6daba7eecc0b26fbe7f93f21c8
hamholla <hamhollett@gmail.com>
2016-07-01 11:50:23 -0400

    get in touch icon and text  change on click, responsiveness of get in touch form, hover tweak for tks logo

--- source/assets/javascripts/getintouch.js
+++ source/assets/javascripts/getintouch.js
@@ -2,6 +2,13 @@ import Velocity from 'velocity-animate'
 
 $(".js-get-in-touch").click(function() {
   $('.getintouch-bar').velocity("scroll", { duration: 750, offset: 250 });
+  var $this = $(this);
+  $this.toggleClass('.js-get-in-touch');
+  if($this.hasClass('.js-get-in-touch')){
+    $this.html('Collapse <i id="iconChange" class="fa fa-arrow-down"></i>');
+  } else {
+    $this.html('Expand <i id="iconChange" class="fa fa-arrow-up"></i>');
+  }
 });
 
 // set up velocity movement instead
