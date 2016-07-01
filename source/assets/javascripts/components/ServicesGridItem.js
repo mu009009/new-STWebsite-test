3e1d1af486af5670fbecec0fd5a8d85a8cdda35d
ben-dalton <benadalton@gmail.com>
2016-06-30 23:20:08 -0400

    adds react-bootstrap popover to services grid

--- source/assets/javascripts/components/ServicesGridItem.js
+++ source/assets/javascripts/components/ServicesGridItem.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Popover, OverlayTrigger } from 'react-bootstrap';
 
 export default class ServicesGrid extends React.Component {
 	render() {
     const { item, index, itemsLength, activeItem } = this.props;
 		return (
+			<OverlayTrigger 
+				trigger="click" 
+				rootClose 
+				positionLeft={-50} 
+				placement="left" 
+				overlay={
+					<Popover 
+						id={`popover_${index}`} 
+						title={item.name}>{item.description}</Popover>}>
 			<div className={`service ${activeItem ? 'active' : 'inactive'} `}>
 				<div className="service-icon">
 					<img src={`assets/images/services/${item.icon}`}/>
@@ -12,6 +22,7 @@ export default class ServicesGrid extends React.Component {
 					{item.name}
 				</div>
 			</div>
+    </OverlayTrigger>
 		);
   }
 }
