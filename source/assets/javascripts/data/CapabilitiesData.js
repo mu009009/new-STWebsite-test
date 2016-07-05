29cdbc9caeb14de58c4bb750bd3a7eb5115191ee
ben-dalton <benadalton@gmail.com>
2016-07-05 14:21:45 -0400

    puts react structure in place for capabilities section

---
+++ source/assets/javascripts/data/CapabilitiesData.js
@@ -0,0 +1,35 @@
+const CapabilitiesData = [
+	{
+		key: 1,
+		name: 'Demand Creation',
+		icon: "demand-generation",
+		categories: ['ALL', 'FUNNEL'],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam diam mi, ut euismod lectus scelerisque quis. Suspendisse faucibus nisl pulvinar augue feugiat, non consequat sapien pretium. Nunc tellus purus.'
+	}, {
+		key: 2,
+		name: 'Marketing & Sales Technology',
+		icon: "marketing_sales-technology",
+		categories: ['ALL', 'EXTEND_REACH'],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam diam mi, ut euismod lectus scelerisque quis. Suspendisse faucibus nisl pulvinar augue feugiat, non consequat sapien pretium. Nunc tellus purus.'
+	}, {
+		key: 3,
+		name: 'Data Services & Data Science',
+		icon: "teleservices",
+		categories: ['ALL'],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam diam mi, ut euismod lectus scelerisque quis. Suspendisse faucibus nisl pulvinar augue feugiat, non consequat sapien pretium. Nunc tellus purus.'
+	}, {
+		key: 4,
+		name: 'Teleservices',
+		icon: "teleservices",
+		categories: ['ALL', 'FUNNEL'],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam diam mi, ut euismod lectus scelerisque quis. Suspendisse faucibus nisl pulvinar augue feugiat, non consequat sapien pretium. Nunc tellus purus.'
+	}, {
+		key: 5,
+		name: 'Incentive, Loyalty & Rebate Program Management',
+		icon: "incentive-loyalty_program-mgmt",
+		categories: ['ALL', 'FUNNEL'],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam diam mi, ut euismod lectus scelerisque quis. Suspendisse faucibus nisl pulvinar augue feugiat, non consequat sapien pretium. Nunc tellus purus.'
+	}
+];
+
+export default CapabilitiesData;
