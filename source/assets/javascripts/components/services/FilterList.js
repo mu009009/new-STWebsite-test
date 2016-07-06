05c56d859280c1228c80cf82ab8e929d430d883d
ben-dalton <benadalton@gmail.com>
2016-07-06 10:03:47 -0400

    adds redux to handle state for capabilities

--- source/assets/javascripts/components/services/FilterList.js
+++ source/assets/javascripts/components/services/FilterList.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import { allFilters, ServicesFilters } from '../data/ServicesFilters';
 
-const FilterList = ({ onToggleFilters, onFilterSelect, selectedFilters }) => {
+const FilterList = ({ onFilterSelect }) => {
 	return (
 		<div className="row">
 			<div className="col-xs-12">
 				<div className="subhead filter-header"> I need to...</div>
 				<div className="filter-list">
-					{ServicesFilters.map(filter => {
-						return (
-							<button 
-								key={filter.key}
-								onClick={() => onFilterSelect(filter.filter)} 
-								className="btn btn-link capabilities-filter">
-								<div className='filter-checkbox'>
-									<img src={`assets/images/services/checkbox-${selectedFilters.indexOf(filter.filter) > -1 ? 'checked' : 'unchecked'}.png`} />
-								</div>
-								<div className="filter-text">
-									{filter.text}
-								</div>
-							</button>
-						)
-					})}
+					<button onClick={() => onFilterSelect('NEW_MARKET')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">01</p>Integrating data from all systems that support our marketing and sales activities.</button>
+					<button onClick={() => onFilterSelect('EXTEND_REACH')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">02</p>Having enough "boots on the ground" to effectively maximize our sales opportunities.</button>
+					<button onClick={() => onFilterSelect('LEADS')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">03</p>Providing marketing and sales support for our distribution channel partners.</button>
+					<button onClick={() => onFilterSelect('FUNNEL')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">04</p>Improving leads -- getting them, qualifying them, and closing them.</button>
+					<button onClick={() => onFilterSelect('GAP')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">05</p>I need purpose-built apps to support my marketing and sales activities.</button>
+					<button onClick={() => onFilterSelect('GAP')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">06</p>Implementing a loyalty program to reward our best customers</button>
 				</div>
-				<button onClick={() => onToggleFilters()} className="btn btn-primary capabilities-filter">{selectedFilters.length == allFilters.length ? "Clear All" : "Show All Services"}</button>
+				<button onClick={() => onFilterSelect('ALL')} className="btn btn-primary capabilities-link">show all services</button>
 			</div>
 		</div>
 	)
--- source/assets/javascripts/components/services/ServicesGrid.js
+++ source/assets/javascripts/components/services/ServicesGrid.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
 import _ from 'lodash';
+import React, { Component } from 'react';
+import FlipMove from 'react-flip-move';
 import ServicesGridItem from './ServicesGridItem';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class ServicesGrid extends Component {
 	render() {
-		const { selectedFilters, services } = this.props;
-		const serviceItems = services.map(function(item) {
+		const { selectedFilter, services } = this.props;
+		const filteredItems = _.sortBy(services, function(o) {
+			return o.categories.indexOf(selectedFilter)>-1;
+		})
+		const serviceItems = _.reverse(filteredItems).map(function(item) {
 			return (
 				<ServicesGridItem
-					activeItem={_.intersection(selectedFilters, item.categories).length > 0}
+					activeItem={item.categories.indexOf(selectedFilter)>-1}
 					item={item}
 					key={item.key}/>
 			);
 		});
 		return (
-			<div className="row">
-				{serviceItems}
+			<div>
+				<FlipMove className="row gridbox" easing="cubic-bezier(0, 0.2, 0.2, 0)">
+					{serviceItems}
+				</FlipMove>
 			</div>
 		)
 	}
