9acef2c423e748162753372408294bbf1077616f
ben-dalton <benadalton@gmail.com>
2016-07-06 10:03:47 -0400

    adds redux to handle state for capabilities

--- source/assets/javascripts/components/capabilities/CapabilitiesGridItem.js
+++ source/assets/javascripts/components/capabilities/CapabilitiesGridItem.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import CapabilityDetails from './CapabilityDetails';
 
 class CapabilitiesGridItem extends Component {
 	render() {
-		const { showDetails, item } = this.props;
-		return (
-			<div onClick={() => showDetails(item)} className="grid__item">
-					<div className="icon-container">
-						<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}.png`} alt={item.name} />
-						<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}-color.png`} alt={item.name} />
-					</div>
-					<div className="loader"><div className="loader-fill"></div></div>
-					<div className="meta meta--preview">
-						<h3 className="title title--preview">
-							{item.name}
-						</h3>
-					</div>
+	const { item, selectCapability } = this.props;
+	return (
+			<a className="grid__item icon-box" onClick={() => selectCapability(item.key)}>
+				<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}.png`} alt={item.name} />
+				<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}-color.png`} alt={item.name} />
+				<div className="loader"></div>
+				<div className="meta meta--preview">
+					<h3 className="title title--preview">
+						{item.name}
+					</h3>
 				</div>
-		)
-	}
+			</a>
+	)}
 };
 
 export default CapabilitiesGridItem;
