94d70a1d4207f3237dfeeefb219277b59efa4d59
ben-dalton <benadalton@gmail.com>
2016-07-06 17:26:21 -0400

    replaces contact form with cognito form

--- source/assets/javascripts/onload.js
+++ source/assets/javascripts/onload.js
@@ -1,5 +1,7 @@
 import Velocity from 'velocity-animate'
 
 $(document).ready(function () {
-  $('body').velocity("fadeIn", { duration: 800 });
+	$('body').velocity("fadeIn", { duration: 800 });
+	$('.cognito form + div').hide();
 });
+
