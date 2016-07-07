354bc977ea0941128f8d90016378b66ee817d150
ben-dalton <benadalton@gmail.com>
2016-07-07 14:43:06 -0400

    add animating width on grid items

--- source/assets/javascripts/components/capabilities/CapabilitiesGridItem.js
+++ source/assets/javascripts/components/capabilities/CapabilitiesGridItem.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
-import CapabilityDetails from './CapabilityDetails';
-import { Motion, spring } from 'react-motion';
 
 class CapabilitiesGridItem extends Component {
 	render() {
-		const { item, selectCapability, dismissCapability, selectedCapability } = this.props;
-		
-		if (selectedCapability === item.key) {
-			return (
-				<Motion defaultStyle={{width: 25}} style={{width: spring(100)}}>
-					{value => 
-						<div>
-							<CapabilityDetails
-								item={item}
-								dismissCapability={dismissCapability}
-								style={`width: ${value.width}%`}
-								motionValue={value.width}/>
-						</div>}
-				</Motion>
-			);
-		}
-
+		const { selectCapability, item } = this.props;
 		return (
-			<a className="col-sm-3 grid__item icon-box" onClick={() => selectCapability(item.key)}>
-				<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}.png`} alt={item.name} />
-				<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}-color.png`} alt={item.name} />
+			<a style={this.props.style} className="grid__item" onClick={() => selectCapability(item.key)}>
+				<div className="icon-container">
+					<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}.png`} alt={item.name} />
+					<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}-color.png`} alt={item.name} />
+				</div>
 				<div className="loader"></div>
 				<div className="meta meta--preview">
 					<h3 className="title title--preview">
