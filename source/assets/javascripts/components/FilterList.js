2595506a3caa3f0469f8e4933d32a1187cc6b482
hamholla <hamhollett@gmail.com>
2016-07-08 10:52:27 -0400

    updated filter list copy

--- source/assets/javascripts/components/FilterList.js
+++ source/assets/javascripts/components/FilterList.js
@@ -6,11 +6,12 @@ const FilterList = ({ onFilterSelect }) => {
 			<div className="col-xs-12">
 				<div className="subhead filter-header"> I need to...</div>
 				<div className="filter-list">
-					<button onClick={() => onFilterSelect('NEW_MARKET')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">01</p>Introduce my product to a new market.</button>
-					<button onClick={() => onFilterSelect('EXTEND_REACH')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">02</p>Extend the reach of my sales force.</button>
-					<button onClick={() => onFilterSelect('LEADS')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">03</p>Deliver more qualified leads to my sales team.</button>
-					<button onClick={() => onFilterSelect('FUNNEL')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">04</p>Grow the funnel.</button>
-					<button onClick={() => onFilterSelect('GAP')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">05</p>Close the gap in field/territory coverage.</button>
+					<button onClick={() => onFilterSelect('NEW_MARKET')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">01</p>Integrating data from all systems that support our marketing and sales activities.</button>
+					<button onClick={() => onFilterSelect('EXTEND_REACH')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">02</p>Having enough "boots on the ground" to effectively maximize our sales opportunities.</button>
+					<button onClick={() => onFilterSelect('LEADS')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">03</p>Providing marketing and sales support for our distribution channel partners.</button>
+					<button onClick={() => onFilterSelect('FUNNEL')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">04</p>Improving leads -- getting them, qualifying them, and closing them.</button>
+					<button onClick={() => onFilterSelect('GAP')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">05</p>I need purpose-built apps to support my marketing and sales activities.</button>
+					<button onClick={() => onFilterSelect('GAP')} className="btn btn-link capabilities-link"><p className=" capabilities-numeral">06</p>Implementing a loyalty program to reward our best customers</button>
 				</div>
 				<button onClick={() => onFilterSelect('ALL')} className="btn btn-primary capabilities-link">show all services</button>
 			</div>
