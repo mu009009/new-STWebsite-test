2b75be0c114b0b5b1828c9ecb77a6f68249df282
ben-dalton <benadalton@gmail.com>
2016-07-08 08:34:54 -0400

    add capability item

--- source/assets/javascripts/data/CapabilitiesData.js
+++ source/assets/javascripts/data/CapabilitiesData.js
@@ -12,19 +12,25 @@ const CapabilitiesData = [
 		categories: ['ALL', 'EXTEND_REACH'],
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam diam mi, ut euismod lectus scelerisque quis. Suspendisse faucibus nisl pulvinar augue feugiat, non consequat sapien pretium. Nunc tellus purus.'
 	}, {
-		key: 3,
-		name: 'Data Services & Data Science',
+		key: 4,
+		name: 'Data Services',
 		icon: "teleservices",
 		categories: ['ALL'],
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam diam mi, ut euismod lectus scelerisque quis. Suspendisse faucibus nisl pulvinar augue feugiat, non consequat sapien pretium. Nunc tellus purus.'
 	}, {
-		key: 4,
+		key: 5,
+		name: 'Data Science',
+		icon: "teleservices",
+		categories: ['ALL'],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam diam mi, ut euismod lectus scelerisque quis. Suspendisse faucibus nisl pulvinar augue feugiat, non consequat sapien pretium. Nunc tellus purus.'
+	}, {
+		key: 6,
 		name: 'Teleservices',
 		icon: "teleservices",
 		categories: ['ALL', 'FUNNEL'],
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam diam mi, ut euismod lectus scelerisque quis. Suspendisse faucibus nisl pulvinar augue feugiat, non consequat sapien pretium. Nunc tellus purus.'
 	}, {
-		key: 5,
+		key: 7,
 		name: 'Incentive, Loyalty & Rebate Program Management',
 		icon: "incentive-loyalty_program-mgmt",
 		categories: ['ALL', 'FUNNEL'],
