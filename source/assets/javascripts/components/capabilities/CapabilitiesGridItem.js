a0faba9225f1a2723992684a057c495265308204
ben-dalton <benadalton@gmail.com>
2016-07-08 15:27:10 -0400

    adds popover as detail view

--- source/assets/javascripts/components/capabilities/CapabilitiesGridItem.js
+++ source/assets/javascripts/components/capabilities/CapabilitiesGridItem.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import Popover from 'material-ui/Popover/Popover';
+import CapabilityDetails from './CapabilityDetails';
 
 class CapabilitiesGridItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      anchorOrigin: {
+        horizontal: 'middle',
+        vertical: 'center',
+      },
+      targetOrigin: {
+        horizontal: 'middle',
+        vertical: 'center',
+      },
+    };
+  }
+
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+		event.preventDefault();
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+	};
+
 	render() {
 		const { selectCapability, item } = this.props;
 		return (
-			<a style={this.props.style} className="grid__item" onClick={() => selectCapability(item.key)}>
+			<a style={this.props.style} className="grid__item" onClick={this.handleTouchTap}>
 				<div className="icon-container">
 					<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}.png`} alt={item.name} />
 					<img className="capabilities-icon center-block" src={`assets/images/capabilities/${item.icon}-color.png`} alt={item.name} />
@@ -15,6 +48,17 @@ class CapabilitiesGridItem extends Component {
 						{item.name}
 					</h3>
 				</div>
+				<Popover
+						className="popover"
+						open={this.state.open}
+						anchorEl={this.state.anchorEl}
+						anchorOrigin={this.state.anchorOrigin}
+						targetOrigin={this.state.targetOrigin}
+						onRequestClose={this.handleRequestClose}>
+						<CapabilityDetails
+							item={item}
+							dismissCapability={this.handleRequestClose}/>
+				</Popover>
 			</a>
 		)
 	}
