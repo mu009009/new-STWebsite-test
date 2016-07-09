3b56c1fc6e523184a7dd75eac91a2269450fb086
ben-dalton <benadalton@gmail.com>
2016-07-09 15:04:21 -0400

    refactors functionality of services grid to allow for multiple filters

---
+++ source/assets/javascripts/data/ServicesFilters.js
@@ -0,0 +1,29 @@
+const ServicesFilters = [
+	{
+		key: 1,
+		text: 'Integrating data from all systems that support our marketing and sales activities.',
+		filter: 'DATA_INTEGRATION'
+	}, {
+		key: 2,
+		text: 'Having enough "boots on the ground" to effectively maximize our sales opportunities.',
+		filter: 'BOOTS'
+	}, {
+		key: 3,
+		text: 'Providing marketing and sales support for our distribution channel partners.',
+		filter: 'SALES_SUPPORT'
+	}, {
+		key: 4,
+		text: 'Improving leads &mdash; getting them, qualifying them, and closing them.',
+		filter: 'LEADS'
+	}, {
+		key: 5,
+		text: 'I need purpose-built apps to support my marketing and sales activities.',
+		filter: 'APPS'
+	}, {
+		key: 6,
+		text: 'Implementing a loyalty program to reward our best customers',
+		filter: 'LOYALTY'
+	}
+];
+
+export default ServicesFilters;
