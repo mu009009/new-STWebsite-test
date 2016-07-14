e18b672ab04c3e376c33f557c03746ffba05adcd
hamholla <hamhollett@gmail.com>
2016-07-14 10:21:05 -0400

    copy updates

--- source/assets/javascripts/components/capabilities/CapabilitiesIntro.js
+++ source/assets/javascripts/components/capabilities/CapabilitiesIntro.js
@@ -6,12 +6,12 @@ export default class CapabilitiesIntro extends Component {
 			<div className="container content-section">
 				<div className="row">
 					<div className="col-md-7">
-						<div className="subhead">What we know</div>
+						<div className="subhead">What we do</div>
 						<div className="highlighted-content-box">
 							<div className="highlighted-content">Capabilities</div>
 						</div>
 						<div className="body-copy">
-							Technekes is filled with talented folks from a range of different fields of work. These are the areas in which we thrive and build upon every day. Click through to read more about each opportunity.
+							While Technekes' capabilities are broad, they're all designed to do one thing: <i> help  you create demand</i>. Take a moment to learn how we can help you attract, acquire and grow customers.
 						</div>
 				</div>
 				</div>
