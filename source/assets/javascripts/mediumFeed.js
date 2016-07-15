80dc572dcb7b4f330885382b6138b36678544694
ben-dalton <benadalton@gmail.com>
2016-07-15 10:13:46 -0400

    add equal heights to rss feed

--- source/assets/javascripts/mediumFeed.js
+++ source/assets/javascripts/mediumFeed.js
@@ -1,4 +1,6 @@
 require('jquery-rss');
+import matchHeight from 'jquery-match-height';
+
 
 if ($('#rss-feeds').length>0) {
 	$("#rss-feeds").rss("http://medium.com/feed/technekes",
@@ -6,7 +8,15 @@ if ($('#rss-feeds').length>0) {
 			limit: 6,
 			layoutTemplate: "<div class='row feed-container'>{entries}</div>",
 			entryTemplate: '<div class="col-md-6 col-lg-4 col-sm-6 feed-item"><a target="_blank" href="{url}"><div class="post-image-container">{teaserImage}</div><div class="post-title highlighted-content">{title}</div><div class="post-body body-copy-small">{shortBodyPlain}...</div></a></div>',
-			ssl: true
+			ssl: true,
+			success: function() {
+				$('.feed-item').matchHeight({
+					byRow: true,
+					property: 'min-height',
+					target: null,
+					remove: false
+				});
+			}
 		}
 	);
 }
