1bdad9f713165855e7ecd6c1ea187090e8b81b7f
ben-dalton <benadalton@gmail.com>
2016-07-15 10:43:34 -0400

    .match-height class available anywhere

--- source/assets/javascripts/onload.js
+++ source/assets/javascripts/onload.js
@@ -13,4 +13,10 @@ $(document).ready(function () {
 			});
 			return false;
 	});
+	$('.match-height').matchHeight({
+		byRow: true,
+		property: 'min-height',
+		target: null,
+		remove: false
+	});
 });
