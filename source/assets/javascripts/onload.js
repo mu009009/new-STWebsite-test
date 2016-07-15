c1ecab0213b8c32fc7ac2f840b6838d64e2f7acf
ben-dalton <benadalton@gmail.com>
2016-07-15 07:10:04 -0400

    replace checkboxes with radio buttons and smooth scroll anchor links

--- source/assets/javascripts/onload.js
+++ source/assets/javascripts/onload.js
@@ -2,5 +2,12 @@ import Velocity from 'velocity-animate'
 
 $(document).ready(function () {
 	$('body').velocity("fadeIn", { duration: 800 });
-});
 
+	$('a.anchor-link').on('click', function(e){
+			e.preventDefault();
+			$('html, body').animate({
+						scrollTop: $( $.attr(this, 'href') ).offset().top
+					}, 500);
+			return false;
+	});
+});
