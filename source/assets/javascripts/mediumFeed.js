fd59b0077f741716d0a1f752b6225773294bc3ef
hamholla <hamhollett@gmail.com>
2016-07-26 16:33:15 -0400

    changed medium from 6 to 9

--- source/assets/javascripts/mediumFeed.js
+++ source/assets/javascripts/mediumFeed.js
@@ -5,7 +5,7 @@ import matchHeight from 'jquery-match-height';
 if ($('#rss-feeds').length>0) {
 	$("#rss-feeds").rss("http://medium.com/feed/technekes",
 		{
-			limit: 6,
+			limit: 9,
 			layoutTemplate: "<div class='row feed-container'>{entries}</div>",
 			entryTemplate: '<div class="col-md-6 col-lg-4 col-sm-6 feed-item"><a target="_blank" href="{url}"><div class="post-image-container">{teaserImage}</div><div class="post-title highlighted-content">{title}</div><div class="post-body body-copy-small">{shortBodyPlain}...</div></a></div>',
 			ssl: true,
