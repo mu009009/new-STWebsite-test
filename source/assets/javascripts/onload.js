ecdac63d0c5858890cec68e722365faee1a33674
hamholla <hamhollett@gmail.com>
2016-10-28 16:33:32 -0400

    media card truncation

--- source/assets/javascripts/onload.js
+++ source/assets/javascripts/onload.js
@@ -9,6 +9,6 @@ $(document).ready(function(){
 		target: null,
 		remove: false
 	});
-
+  $('.c-forms-form').parent().next().css("display", "none");
   $('.c-forms-form').parent().next().hide();
 });
