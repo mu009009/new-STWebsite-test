11694e6d89b00ea7a29efa6b1170754ceeedc90a
Kula <apple@apple-MacB.local>
2016-12-08 11:11:37 -0500

    auto fill, urlencode, typo, congratulation page, horizontal contact form

--- source/assets/javascripts/onload.js
+++ source/assets/javascripts/onload.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
   window.setTimeout( () => { $('#fouc').fadeIn(1000) } , 500);
 
   $('.match-height').matchHeight({
-		byRow: true,
-		property: 'min-height',
-		target: null,
-		remove: false
-	});
+    byRow: true,
+    property: 'min-height',
+    target: null,
+    remove: false
+  });
   $('.c-forms-form').parent().next().css("display", "none");
   $('.c-forms-form').parent().next().hide();
 });
